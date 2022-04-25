34ce88e2fee912f587a11897c96d25123ca8cd9d
lyee197 <liamsyee@gmail.com>
2022-04-25 11:11:31 -0400

    Added Pet model

---
+++ app/models/pet.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const petSchema = new mongoose.Schema(
+    {
+        owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+        birthday: {
+            type: Date,
+            required: true,
+        },
+        animal_type: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Pet', petSchema)
