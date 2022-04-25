366f6f26ddff8d47f728e97407fffaceff0e6969
lyee197 <liamsyee@gmail.com>
2022-04-25 16:06:32 -0400

    Created commentSchema

---
+++ app/models/comment.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema(
+    {
+        owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+        comment: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+		timestamps: true,
+	}
+)
+
+module.exports = commentSchema
